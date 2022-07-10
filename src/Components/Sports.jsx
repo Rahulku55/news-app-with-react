62eb0826de240325f1b45ee42ebc21faa08f83a9
Rahul kumar <rk857963@gmail.com>
2022-07-10 21:08:38 +0530

    news projects

---
+++ src/Components/Sports.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+
+
+export default function India() {
+    const [value, setValue] = useState("");
+    async function getNews() {
+
+        let response = await fetch("https://api.newscatcherapi.com/v2/latest_headlines?countries=IN&lang=en&topic=sport", {
+            headers: {
+                'x-api-key': 'bxnwiWww3uVsmDB7MwBoTXihAohKFlo1_8I_vwzp104'
+            }
+        });
+
+        let data = await response.json();
+
+        let news = data.articles.map((a, i) => {
+            return (
+                <>
+                    <Card title={a.title} desc={a.excerpt} key={i} urlToImage={a.media} url={a.link} />
+                </>
+            );
+        })
+        setValue(news)
+
+    }
+    useEffect(() => {
+        getNews()
+    }, [])
+
+    return (
+        <>
+            <main>
+                <h1>Sports</h1>
+                <div className="news-section">
+                    {value}
+                </div>
+            </main>
+        </>
+    )
+}
